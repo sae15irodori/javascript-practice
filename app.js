8b842ad4369ecba9b415f786d6b48e0b9dd1fc6a
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-08 03:21:52 +0000

    Add letのスコープ

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var str = "webcamp" //strがグローバルスコープ
-
 function foo() {
-  console.log(str)
-  var y ="hello"　　//yが関数スコープとなっている
+  let x = "webcamp"
+  {
+    let y = "hello webcamp"
+  }
+  console.log(x)
+  console.log(y)
 }
 
 foo()
-console.loge(y)　//yはfunction{}の中でしか使えないからエラーとなる
+
+//foo()という名前の関数の中に変数xとyがあり、それぞれコンソールログに出力
+//ただし、変数ｙが宣言した｛｝より外で呼び出されているのでエラーとなってしまう
