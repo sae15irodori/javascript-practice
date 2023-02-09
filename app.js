260eca8117439ed3e77690da557f46df6763b2de
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-02-09 06:16:16 +0000

    Add 関数

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-for (let i = 0; i < 10; i++){
-  console.log(i)
+let alertString;                   //変数alertStringを宣言
+alertString = addString("WebCamp");//変数の中身はaddString関数。引数も指定。
+alert(alertString);               //アラートにalertString変数の中身表示させる
+
+
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
 }
-
-console.log(i)
-
-//変数iはfor文内の（）ブロック内で宣言されたもの
-//それをブロック外から参照したからエラーになった。
-//letによる変数宣言では、宣言されたブロック内が有効範囲となる
